5ba71d2a250f2575bd5fa3753b2088921aa8d125
Maylson <maylsonmoraesd@hotmail.com>
2022-02-08 10:22:46 -0300

    upload de arquivos

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {uuid} = require('uuidv4');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+
+const uploadsFolder = path.resolve(__dirname, './uploads');
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: uploadsFolder,
+        filename:(req,file,callback) => callback(null, uuid() + path.extname(file.originalname))
+    })
+})
+
+app.use('/file', express.static(uploadsFolder))
+
+app.post('/file',upload.single('imagem'),(req,res) => {
+    res.send('Deu certo');
+})
+
+app.listen(3333);
